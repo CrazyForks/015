9aa52915b03d394e6443a1d6274707e8153c8b9c
keven1024 <keven@fudaoyuan.icu>
2026-02-28 10:23:45 +0800

    feat(front): implement ImageConvertResult component for image format conversion and update layout in ImageCompressResult

--- front/components/Result/ImageConvertResult.vue
+++ front/components/Result/ImageConvertResult.vue
@@ -6,7 +6,6 @@ import { filesize } from 'filesize'
 import useMyAppShare from '@/composables/useMyAppShare'
 import { toast } from 'vue-sonner'
 import type { handleFileComponentProps } from './types'
-import { get } from 'lodash-es'
 
 const { t } = useI18n()
 const emit = defineEmits<{
@@ -14,8 +13,9 @@ const emit = defineEmits<{
 }>()
 const props = defineProps<handleFileComponentProps>()
 const fileIds = computed(() => props?.data?.files?.map((item) => item.id))
+const targetExt = computed(() => props?.data?.config?.target_ext)
 const { data: taskIds } = useQuery({
-    queryKey: ['create-image-compress', fileIds.value],
+    queryKey: ['create-image-convert', fileIds.value, targetExt.value],
     queryFn: async () => {
         return await Promise.all(
             props?.data?.files?.map(async (file) => {
@@ -26,10 +26,11 @@ const { data: taskIds } = useQuery({
                     data: {
                         id?: string
                     }
-                }>(`/api/task/image:compress`, {
+                }>(`/api/task/image:convert`, {
                     method: 'POST',
                     body: {
                         file_id: id,
+                        target_ext: targetExt.value,
                     },
                 })
                 return data?.data?.id
@@ -45,7 +46,7 @@ const taskResults = useQueries({
         () =>
             taskIds?.value?.filter(Boolean).map((taskId) => {
                 return {
-                    queryKey: ['task-image-compress', taskId],
+                    queryKey: ['task-image-convert', taskId],
                     queryFn: async () => {
                         const data = await $fetch<{
                             code: number
@@ -76,19 +77,6 @@ const taskResults = useQueries({
     ),
 })
 
-const totalSize = computed(() => {
-    return taskResults.value.reduce(
-        (acc, item) => {
-            const { new_file, old_file } = get(item, 'data.result.0') || {}
-            return {
-                oldSize: acc.oldSize + (old_file?.size ?? 0),
-                newSize: acc.newSize + (new_file?.size ?? 0),
-            }
-        },
-        { oldSize: 0, newSize: 0 }
-    )
-})
-
 const { downloadFileByShareId, createFileShare } = useMyAppShare()
 
 const { counter, pause } = useInterval(2000, { controls: true })
@@ -106,29 +94,32 @@ watch(
         }
     }
 )
+
+const totalExt = computed(() => {
+    return props?.data?.files?.reduce<string[]>((acc, item) => {
+        const [, originalName, originalExt] = item?.file?.name?.match(/(.*?)\.([^.]+(?:\.[^.]+)*)$/) ?? []
+        if (originalExt) acc.push(originalExt)
+        return acc
+    }, [])
+})
 </script>
 <template>
-    <BaseCard class="flex flex-col gap-3" :title="t('page.result.imageCompress.title')" :showBackButton="true">
+    <BaseCard class="flex flex-col gap-3" :title="t('page.result.imageConvert.title')" :showBackButton="true">
         <div class="flex flex-row gap-3">
             <div class="rounded-xl flex flex-col bg-white/70 px-3 py-2 gap-1 basis-2/3">
-                <div class="text-sm font-semibold">{{ t('page.result.imageCompress.totalSize') }}</div>
+                <div class="text-sm font-semibold">{{ t('page.result.imageConvert.convert') }}</div>
                 <div class="text-2xl font-light flex flex-row items-center gap-1">
-                    <span v-if="totalSize.oldSize > 0" class="opacity-75">{{ filesize(totalSize.oldSize) }}</span>
-                    <span v-else><Skeleton class="w-12 h-10" /></span>
+                    <div v-for="ext in totalExt" class="rounded flex flex-row items-center bg-primary/20 text-primary p-1 py-0.5 text-sm">
+                        {{ ext?.toUpperCase() }}
+                    </div>
                     <LucideChevronsRight class="size-6" />
-                    <span v-if="totalSize.newSize > 0">{{ filesize(totalSize.newSize) }}</span>
-                    <span v-else><Skeleton class="w-12 h-10" /></span>
-                    <div
-                        v-if="totalSize.oldSize > 0 && totalSize.newSize > 0"
-                        class="rounded flex flex-row items-center bg-green-100 text-green-600 p-1 py-0.5 text-sm"
-                    >
-                        <LucideArrowDown class="size-4" />
-                        {{ ((1 - totalSize.newSize / totalSize.oldSize) * 100).toFixed(2) }}%
+                    <div class="rounded flex flex-row items-center bg-primary/20 text-primary p-1 py-0.5 text-sm">
+                        {{ targetExt?.toUpperCase() }}
                     </div>
                 </div>
             </div>
             <div class="rounded-xl flex flex-col bg-white/70 px-3 py-2 gap-1 basis-1/3">
-                <div class="text-sm font-semibold">{{ t('page.result.imageCompress.task') }}</div>
+                <div class="text-sm font-semibold">{{ t('page.result.imageConvert.task') }}</div>
                 <div class="text-3xl font-light">{{ taskResults.length }}</div>
             </div>
         </div>
@@ -148,27 +139,19 @@ watch(
             <div class="flex flex-row gap-1 items-center text-sm" v-if="taskResults?.[index]?.data?.status === 'retry'">
                 <LucideLoader2 class="size-4 animate-spin" />
                 {{
-                    t('page.result.imageCompress.retry', [
+                    t('page.result.imageConvert.retry', [
                         taskResults?.[index]?.data?.err?.retry ?? 0,
                         taskResults?.[index]?.data?.err?.max_retry ?? 0,
                     ])
                 }}
             </div>
             <div class="flex items-center justify-center" v-if="taskResults?.[index]?.data?.status === 'archived'">
-                <div class="text-sm text-red-500 px-2 py-1 rounded-md bg-red-100">{{ t('page.result.imageCompress.failed') }}</div>
+                <div class="text-sm text-red-500 px-2 py-1 rounded-md bg-red-100">{{ t('page.result.imageConvert.failed') }}</div>
             </div>
             <div class="flex flex-row gap-2 items-center shrink-0" v-if="taskResults?.[index]?.data?.status === 'success'">
                 <div class="flex flex-col gap-1 items-center">
-                    <div class="rounded flex flex-row items-center bg-green-100 text-green-600 px-1 text-xs">
-                        <LucideArrowDown class="size-4" />
-                        {{
-                            (
-                                (1 -
-                                    (taskResults?.[index]?.data?.result?.[0]?.new_file?.size ?? 0) /
-                                        (taskResults?.[index]?.data?.result?.[0]?.old_file?.size ?? 0)) *
-                                100
-                            ).toFixed(2)
-                        }}%
+                    <div class="rounded flex flex-row items-center bg-primary/20 text-primary px-1 text-xs">
+                        {{ targetExt.toUpperCase() }}
                     </div>
                     <div class="text-xs opacity-50">{{ filesize(taskResults?.[index]?.data?.result?.[0]?.new_file?.size ?? 0) }}</div>
                 </div>
@@ -178,8 +161,9 @@ watch(
                         async () => {
                             const { new_file } = taskResults?.[index]?.data?.result?.[0] || {}
                             if (!new_file?.id) return
+                            const [, originalName, originalExt] = item?.file?.name?.match(/(.*?)\.([^.]+(?:\.[^.]+)*)$/) ?? []
                             const data = await createFileShare({
-                                files: [{ id: new_file?.id as string, name: item?.file?.name }],
+                                files: [{ id: new_file?.id as string, name: `${originalName}.${targetExt}` }],
                                 config: {
                                     download_nums: 1,
                                     expire_time: 60,
